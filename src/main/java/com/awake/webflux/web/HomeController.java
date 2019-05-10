93af9221f87ffd65586a8502cf7935914ed84ecd
周辉 <hui.zhou@lanmaoly.com>
2019-05-10 15:11:50 +0800

    简单demo

--- src/main/java/com/awake/webflux/web/HomeController.java
+++ src/main/java/com/awake/webflux/web/HomeController.java
@@ -1,13 +1,13 @@
 package com.awake.webflux.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/index")
+    @GetMapping("/index")
     public Mono<String> homePage(){
         return Mono.just("hello flux");
     }
